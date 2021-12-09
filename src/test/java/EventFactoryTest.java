a7d8283537e81ab9ed9f72eb5a8835988fcd7896
Cathlyn <cathlynchen18@gmail.com>
2021-12-08 19:28:55 -0500

    Added more tests for EventFactory

--- src/test/java/EventFactoryTest.java
+++ src/test/java/EventFactoryTest.java
@@ -8,24 +8,48 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class EventFactoryTest {
-    EventFactory e;
+    EventFactory f;
 
     @Before
     public void setUp() {
-        e = new EventFactory();
+        f = new EventFactory();
     }
 
     @Test(timeout = 50)
     public void TestEvent() {
-        CalendarEvent event = e.createEvent("Lecture", 3.0f, 4.0f, "Tuesday");
+        CalendarEvent event = f.createEvent("Lecture", 3.0f, 4.0f, "Tuesday");
         assertEquals(3.0f, event.getStartTime(), 0);
     }
 
+    @Test(timeout = 50)
+    public void TestEventsCompare() {
+        CalendarEvent e1 = f.createEvent("Lecture", 15.0f, 16.0f, "Tuesday");
+        OneOffEvent e2 = f.createEvent("Interview", 9.0f, 10.0f, 12.09f);
+        assertEquals(1, e1.compareTo(e2));
+    }
 
     @Test(timeout = 50)
     public void TestOneOff() {
-        OneOffEvent event = e.createEvent("Interview", 9.0f, 10.0f, 12.09f);
+        OneOffEvent event = f.createEvent("Interview", 9.0f, 10.0f, 12.09f);
         assertEquals("Interview", event.getName());
+    }
+
+    @Test(timeout = 50)
+    public void TestEventAlter() {
+        CalendarEvent e1 = f.createEvent("Lecture", 15.0f, 16.0f, "Tuesday");
+        assertEquals("Lecture", e1.getName());
+        e1.setName("Tutorial");
+        assertEquals("Tutorial", e1.getName());
+        CalendarEvent e2 = f.createEvent("Lecture", 16.0f, 17.0f, "Tuesday");
+        assertEquals("Lecture", e2.getName());
+    }
+
+    @Test(timeout = 50)
+    public void TestOneOffAlter() {
+        OneOffEvent event = f.createEvent("Hiking", 13.0f, 15.0f, 12.12f);
+        event.setStartTime(12.0f);
+        assertEquals(12.0f, event.getStartTime(), 0);
+        assertEquals(3.0f, event.getDuration(), 0);
     }
 
 }
