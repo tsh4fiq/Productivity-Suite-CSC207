e11b5a90b2985d97b0126d54fd4c9413f61f9c41
Cathlyn <cathlynchen18@gmail.com>
2021-12-08 20:11:36 -0500

    Added more tests for CalendarController

--- src/test/java/CalendarControllerTest.java
+++ src/test/java/CalendarControllerTest.java
@@ -3,6 +3,7 @@ import constants.VarExceptions;
 import events.CalendarEvent;
 import events.OneOffEvent;
 import org.junit.*;
+import users.students.Student;
 import users.students.StudentController;
 
 import java.util.ArrayList;
@@ -22,8 +23,8 @@ public class CalendarControllerTest {
         s.addNewStudent("mike", "password1");
         c = new CalendarController(s);
 
-        recurArr = new ArrayList<CalendarEvent>();
-        singleArr = new ArrayList<OneOffEvent>();
+        recurArr = new ArrayList<>();
+        singleArr = new ArrayList<>();
     }
 
     @Test(timeout = 50)
@@ -44,8 +45,38 @@ public class CalendarControllerTest {
         singleArr.add(single);
         c.addOneOffEvent("mike", singleArr);
         assertEquals(1, c.getStudent("mike").getStudentSchedule().getSingle().get(12.09f).size());
+    }
 
+    @Test(timeout = 50)
+    public void TestCreateRec() {
+        CalendarEvent event = c.createRecEvent("Lecture", 3.0f, 4.0f, "Tuesday");
+        assertEquals(4.0f, event.getEndTime(), 0);
+    }
 
+    @Test(timeout = 50)
+    public void TestCreateOneOff() {
+        OneOffEvent event = c.createOneOffEvent("Fishing", 9.0f, 12.0f, 12.30f);
+        assertEquals(12.0f, event.getEndTime(), 0);
+    }
+
+    @Test(timeout = 50)
+    public void TestStudent() {
+        Student stu = c.getStudent("mike");
+        assertEquals("mike", stu.getUsername());
+    }
+
+    @Test(timeout = 50)
+    public void TestCheckRec() throws VarExceptions {
+        Student student = new Student("beck", "1234");
+        CalendarEvent e = c.createRecEvent("Lecture", 3.0f, 4.0f, "Tuesday");
+        c.checkAvailableRecurring(student, e);
+    }
+
+    @Test(timeout = 50)
+    public void TestCheckOne() throws VarExceptions {
+        Student student = new Student("beck", "1234");
+        OneOffEvent e = c.createOneOffEvent("Climbing", 11.0f, 15.0f, 12.24f);
+        c.checkAvailableOneoff(student, e);
     }
 
 }
